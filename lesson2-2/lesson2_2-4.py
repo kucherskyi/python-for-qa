ffcfa80e841d93ac16650eb5ff984b41848357d8
kucherskyy <a.kucherskyy@gmail.com>
2016-05-11 15:53:30 +0300

    Done

--- lesson2-2/lesson2_2-4.py
+++ lesson2-2/lesson2_2-4.py
@@ -2,3 +2,8 @@
 Write program to evaluate (a or not b) and (c or not a) expression for boolean
 varibles a, b, c showing result for all possible variables combinations.
 """
+
+for a in (True, False):
+    for b in (True, False):
+        for c in (True, False):
+            print ((a or not b) and (c or not a))
